b0d2d25a469cab9e96af439a38255378770bba9f
Tom Gobich <fallofmars@gmail.com>
2016-11-02 14:34:45 -0400

    seperated authentication functionalities into seperate controller and factory files

---
+++ js/controllers/authentication.js
@@ -0,0 +1,27 @@
+'use strict';
+
+(function () {
+
+	'use strict';
+
+	angular.module('app').controller('AuthenticationController', function (AuthenticationFactory) {
+
+		var vm = this;
+
+		vm.user = AuthenticationFactory.getAuthenticatedUser();
+
+		vm.authenticateUser = function (isValid, authenticationForm) {
+			if (isValid) {
+				vm.user.loginDate = Date.now();
+
+				vm.user.isAuthenticated = true;
+
+				AuthenticationFactory.setAuthenticatedUser(vm.user);
+			}
+		};
+
+		vm.logoutUser = function () {
+			vm.user = AuthenticationFactory.unauthenticateUser();
+		};
+	});
+})();
